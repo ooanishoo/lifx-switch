e6032cd3b8120504ce212f57ef13b90916c5c371
ooanishoo <anish.maharjan12341@gmail.com>
2020-03-10 15:55:04 +1100

    Add components folder to seperate from the main file

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Provider as PaperProvider, DefaultTheme } from "react-native-paper";
+import Header from "./src/components/Header";
+import Body from "./src/components/Body";
+
+export default function App() {
+  return (
+    <PaperProvider theme={theme}>
+      <Header />
+      <Body />
+    </PaperProvider>
+  );
+}
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "black",
+    accent: "yellow"
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 50,
+    width: "100%"
+  }
+});
