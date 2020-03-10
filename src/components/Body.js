995ded3b4f8a238874dd5b2317ff4c2e3d7a42bf
ooanishoo <anish.maharjan12341@gmail.com>
2020-03-10 15:54:27 +1100

    Add `react-native-paper` package

---
+++ src/components/Body.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Card, Title, Paragraph } from "react-native-paper";
+
+export default function Body() {
+  return (
+    <Card>
+      <Card.Content>
+        <Title>Card title</Title>
+        <Paragraph>Card content</Paragraph>
+      </Card.Content>
+    </Card>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Appbar} from 'react-native-paper'
+
+export default function Header() {
+  return (
+    <Appbar.Header>
+      <Appbar.Content 
+        title='LIFX'
+      />
+    </Appbar.Header>
+  )
+}
